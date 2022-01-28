6f11104c11c57a391ab04c84d6d0ccab92e039d4
yeabsiramoges <yeab@mit.edu>
2022-01-28 16:08:55 -0500

    iterativey calculated output

--- chapter2/layer.py
+++ chapter2/layer.py
@@ -1,16 +1,22 @@
 inputs = [1.0, 2.0, 3.0, 2.5]
 
-weights1 = [0.2, 0.8, -0.5, 1]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+biases = [2, 3, 0.5]
 
-output = [
-			inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1, 
-			inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2, 
-			inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
-		] #the output is the sum of the produtc of all the inputs and weights, added to the bias
-print(output)
+# output of current layer
+layer_output = []
+
+# for each neuron in the layer
+for n_weights, n_biases in zip(weights, biases):
+	# zeroes out output for given neuron
+	output = 0
+	# loop through each input and weight for the neuron
+	for n_input, weight in zip(inputs, n_weights):
+		#multiply input by associated weight and add to neurons output value
+		output += n_input*weight
+	output += n_biases
+	layer_output.append(output) 
+
+print(layer_output)
+#this is a fully connected neuron. every neuron is connected to every input
--- chapter2/neuron.py
+++ chapter2/neuron.py
@@ -1,6 +1,7 @@
 inputs = [1.0, 2.0, 3.0, 2.5] #made up inputs for the beginning. data passed into the model to get desired outpus
 weights = [0.2, 0.8, -0.5, 1.0] #weights corresponding to each input that are typically set randomly and change throughout the training of the network
-bias = 2.0 #one bias per neuron, similarly, the bias changes as the network is traiend
+bias = [2.0] #one bias per neuron, similarly, the bias changes as the network is traiend
+
 
 output = (
 		inputs[0]*weights[0] + 
